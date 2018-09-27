7f4b4ea5b4375848b21c5f7cbbd1bad5204781d4
Adolphoramirez <adam.adreleve@gmail.com>
2018-09-27 15:01:15 +0200

    Add files via upload

---
+++ MD5.py
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+from hashlib import md5
+from timeit import default_timer
+a = default_timer()
+b = raw_input("Entrer le hash md5:").strip()
+for c in open(raw_input("Choissez le dictionnaire: "), "r"):
+    d =  md5()
+    d.update(str(c.strip()))
+    if b in d.hexdigest():
+        print "Hash crack: %s\n Completed in %s seconds." %(c, int(default_timer()-a)); exit()
+print "Terminé en %s seconds.\n[ ! ] Hash non cracké !" %(int(default_timer()-a)); exit()
